372dd3d4d2bbd52c32d6a126deaf58f7cbbe94c9
developmentHF <development.henry@gmail.com>
2023-02-05 17:32:25 +1100

    made grid paintable + hover effect

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-// make flex container flex col.
-// make rows, which are flex containers going horizontal
-// make 16 elems whcih slot into row containers
-// make another row and add it to container
-// repeat
+// make the paint function draggable
+
 
 const COLUMNS = 16; 
 const ROWS = 16;
 
 const canvas = document.querySelector('#canvas');
 
-// give each element class for row and col
-for(let j=0; j<ROWS; j++) {
-    const newRow = document.createElement('div');
-    newRow.style.display = 'flex';
-    newRow.style.flex = 1;
-    
-    for (let i=0; i <COLUMNS; i++) {
-        const element = document.createElement('div');
-        element.style.minHeight = "20px";
-        element.style.minWidth = "20px";
-        element.style.flex = 1;
-        // element.textContent = `${i}`;
-        element.style.borderStyle = 'solid';
-        element.style.borderWidth = '2px';
-        element.style.backgroundColor = 'gray';
-        newRow.appendChild(element);
+const generateGrid = function(rows, cols, location) {
+    // give each element class for row and col
+    for(let j=0; j<rows; j++) {
+        const newRow = document.createElement('div');
+        newRow.style.display = 'flex';
+        newRow.style.flex = 1;
+        
+        for (let i=0; i <cols; i++) {
+            const element = document.createElement('div');
+            element.classList.add("pixel");
+            element.classList.add("unpainted");
+            newRow.appendChild(element);
+        }
+        location.appendChild(newRow);
     }
+} 
 
-    canvas.appendChild(newRow);
+generateGrid(ROWS, COLUMNS, canvas);
+
+// Colours in pixel after being clicked
+const paint = function(pixel) {
+    pixel.classList.toggle("unpainted");
+    pixel.classList.toggle("painted");
 }
+
+// add listener for paint to all pixels
+const pixels = document.querySelectorAll(".pixel");
+
+pixels.forEach((pixel) => {
+    pixel.addEventListener('click', () => {
+        paint(pixel);
+    })
+})
