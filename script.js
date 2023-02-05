e3e2253cf647816d9b5e1b03e3a02a6a305ae7c1
developmentHF <development.henry@gmail.com>
2023-02-05 18:46:43 +1100

    add grid size changer and eraser

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// make the paint function draggable
-
-
-const COLUMNS = 16; 
-const ROWS = 16;
+// Defines initial values of grid
+let COLUMNS = 16; 
+let ROWS = COLUMNS;
+let isMouseDown = false;
 
 const canvas = document.querySelector('#canvas');
 
+// Generates a grid using default values
 const generateGrid = function(rows, cols, location) {
-    // give each element class for row and col
     for(let j=0; j<rows; j++) {
         const newRow = document.createElement('div');
+        newRow.classList.add("row");
         newRow.style.display = 'flex';
         newRow.style.flex = 1;
         
@@ -24,23 +24,6 @@ const generateGrid = function(rows, cols, location) {
     }
 } 
 
-generateGrid(ROWS, COLUMNS, canvas);
-
-// Colours in pixel after being clicked
-const paint = function(pixel) {
-    pixel.classList.toggle("unpainted");
-    pixel.classList.toggle("painted");
-}
-
-// add listener for paint to all pixels
-const pixels = document.querySelectorAll(".pixel");
-
-pixels.forEach((pixel) => {
-    pixel.addEventListener('click', () => {
-        paint(pixel);
-    })
-})
-
 // add listener for grid toggle
 const gridToggle = document.querySelector("#gridToggle");
 gridToggle.addEventListener('click', () => {
@@ -48,3 +31,58 @@ gridToggle.addEventListener('click', () => {
         pixel.classList.toggle("gridBorder");
     })
 })
+
+// Allows for draggable drawing
+document.addEventListener('mousedown', () => isMouseDown = !isMouseDown);
+document.addEventListener('mouseup', () => isMouseDown = !isMouseDown);
+
+// Add eraser button listener
+isEraserActive = false;
+const eraser = document.querySelector("#eraser");
+eraser.addEventListener('click', () => isEraserActive = !isEraserActive);
+
+// Colours in pixel after being clicked
+const paint = function(pixel) {
+    if (isEraserActive) {
+        pixel.classList.add("unpainted");
+        pixel.classList.remove("painted");
+    } else {
+        pixel.classList.add("painted");
+        pixel.classList.remove("unpainted");
+    }
+}
+
+// Give functionality to change grid size button
+const changeSize = document.querySelector("#gridSize");
+
+changeSize.addEventListener('click', () => {
+    canvas.innerHTML = "";
+    ROWS = prompt("Enter a new side length:");
+    COLUMNS = ROWS;
+    generateGrid(ROWS, COLUMNS, canvas);
+
+    // have to re-add the listeners for new pixel elements
+    generatePixelListeners();
+});
+
+// Lets pixels listen for click and drag events on it
+const generatePixelListeners = function() {
+    // add listener for paint to all pixels
+    const pixels = document.querySelectorAll(".pixel");
+
+    pixels.forEach((pixel) => {
+        pixel.addEventListener('mousedown', () => {
+            paint(pixel);
+        })
+
+        pixel.addEventListener('mouseenter', () => {
+            if (isMouseDown) {
+                paint(pixel);
+            }
+        })
+    })
+}
+
+// Generate default grid to be shown
+generateGrid(ROWS, COLUMNS, canvas);
+generatePixelListeners();
